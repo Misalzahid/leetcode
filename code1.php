26bab253d2b2f41bd67e603e65af29b09da13088
Misalzahid <143399501+Misalzahid@users.noreply.github.com>
2023-10-21 18:03:12 +0500

    Create code1.php

---
+++ code1.php
@@ -0,0 +1,17 @@
+<?php
+function arrayPairSum($nums) {
+    sort($nums); 
+    print_r( $nums);
+
+    $num=count($nums);
+    $sum = 0;
+
+    for ($i = 0; $i < $num; $i += 2) {
+        $sum = $sum + $nums[$i];
+        echo("Current Sum: $sum\n");
+    }
+    echo("Final Sum: $sum\n");
+    //return $sum;
+}
+
+arrayPairSum([6, 2, 6, 5, 1, 2]);
